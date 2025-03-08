29e4b721b7829d20210d46e31fc3ac434934d04e
Michael Hr <michaelhr@MacBook-Air-Michael.local>
2025-03-08 23:46:00 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-a = int(input("Enter num 1:"))
-b = float(input("Enter num 2:"))
+num = int(input("Enter num:"))  
 
-print(a+b)
+if num >= 55:
+    print("Yes")
+    if num == 100:
+        print("Num is 100")
+    elif num == 40:
+        print("Num is 40")
+    elif num == 46:
+        print("Num is 46")
+    elif num < 4:
+        print("Num is 4")
+    else:
+        print("Else")
+        isHappy = True  
+
+if 'isHappy' in locals() and not isHappy:
+    print("Yes, he is happy")
