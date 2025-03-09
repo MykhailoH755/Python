1a3ab655793fc3c61da533c7d0f1a89a7084da1c
Michael Hr <michalhrib20@gmail.com>
2025-03-09 23:20:20 +0100

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-num = int(input("Enter num:"))  
-
-if num >= 55:
-    print("Yes")
-    if num == 100:
-        print("Num is 100")
-    elif num == 40:
-        print("Num is 40")
-    elif num == 46:
-        print("Num is 46")
-    elif num < 4:
-        print("Num is 4")
+for i in "Hello world":
+    if i == "l":
+        print("Done")
+        break
     else:
-        print("Else")
-        isHappy = True  
+        print("Not found")
 
-if 'isHappy' in locals() and not isHappy:
-    print("Yes, he is happy")
