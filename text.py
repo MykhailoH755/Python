8214d424e8ad41bd005f442bdb2ac36e484d9aaa
Michael Hr <michalhrib20@gmail.com>
2025-03-11 23:09:19 +0100

    написав програму

---
+++ text.py
@@ -0,0 +1,11 @@
+text = 'football, basketball, skate'
+hobbies = text.split(',')
+
+for i in range(0, len(hobbies)):
+    hobbies[i] = hobbies[i].capitalize()
+
+
+result =  ","  .join(hobbies)
+
+
+print(result)
